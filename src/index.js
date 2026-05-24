3bd82cfc72a169c57a64b6aa0bdb2e05979dd5f6
dwidegen <dwiandikayulianto3078@gmail.com>
2026-05-24 20:20:27 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';   // ← path benar, cari App di folder yang sama (src)
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
